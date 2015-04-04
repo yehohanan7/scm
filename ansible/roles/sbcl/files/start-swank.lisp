b757f29423786c0121cb3a64381ebb32b19b454b
John Pradeep <yehohanan7@Johns-MacBook-Pro.local>
2015-04-04 03:12:16 +0100

    clean up

--- ansible/roles/sbcl/files/start-swank.lisp
+++
@@ -1,3 +0,0 @@
-(ql:quickload :swank)
-(setf swank::*LOOPBACK-INTERFACE* "0.0.0.0")
-(swank:create-server :port 4005 :dont-close t)
--- ansible/roles/sbcl/files/swank.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-source "//.profile" && cd /sbcl_lib && sbcl --load start-swank.lisp
